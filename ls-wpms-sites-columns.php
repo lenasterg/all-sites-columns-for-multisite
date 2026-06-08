31bd6ae5f2c1dcb50a9ef4da735b8ccec9f18b40
Lena Stergatou <lenasterg@users.noreply.github.com>
2026-06-08 17:54:12 +0300

    Update plugin metadata and enhance text escaping
    Updated plugin metadata and improved text escaping for better security and internationalization.

--- ls-wpms-sites-columns.php
+++ ls-wpms-sites-columns.php
@@ -2,46 +2,53 @@
 /**
  * @package All Sites Columns for Multisite
  */
+ 
 /**
  *  Plugin Name: All Sites Columns for Multisite
- *  Plugin URI: https://wordpress.org/plugins/all-sites-columns-for-multisite
+ *  Plugin URI:  https://wordpress.org/plugins/all-sites-columns-for-multisite/
  *  Description: For Multisite only. The plugin adds sortable columns to the sites network admin table.
- *  Version: 1.0.0
- *  Author: lenasterg, nts.cti.gr, sch.gr
- *  Author URI: http://nts.cti.gr/
- *  License: GPLv2
+ *  Version:     1.0.1
+ *  Author:      lenasterg
+ *  Author URI:  https://infra-nets.cti.gr/el/
+ *  License:     GPLv2
  */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 add_filter( 'wpmu_blogs_columns', 'lsscms_add_sites_column_heading', 99 );
 
 /**
- * Adds the columns headings
+ * Adds the columns headings.
  *
- * @param  array $columns
+ * @param array $columns Existing columns.
  * @return array
  */
 function lsscms_add_sites_column_heading( $columns ) {
 	$x = array(
 		'cb'      => $columns['cb'],
-		'blog_id' => __( 'Blog ID' ),
+		'blog_id' => esc_html__( 'Blog ID' ),
 	);
 
 	$columns_new             = array_merge( $x, $columns );
-	$columns_new['public']   = __( 'Public' );
-	$columns_new['deleted']  = __( 'Deleted' );
-	$columns_new['archived'] = __( 'Archived' );
-	$columns_new['mature']   = __( 'Mature' );
-	$columns_new['spam']     = __( 'Spam' );
-	$columns_new['site_id']  = __( 'Site ID' );
+	$columns_new['public']   = esc_html__( 'Public' ); // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+	$columns_new['deleted']  = esc_html__( 'Deleted' ); // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+	$columns_new['archived'] = esc_html__( 'Archived' ); // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+	$columns_new['mature']   = esc_html__( 'Mature' ); // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+	$columns_new['spam']     = esc_html__( 'Spam' ); // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+	$columns_new['site_id']  = esc_html__( 'Site ID', 'all-sites-columns-for-multisite' );
+
 	return $columns_new;
 }
 
 add_filter( 'manage_sites-network_sortable_columns', 'lsscms_columns_sortable' );
 
 /**
- * Adds the sortable columns
+ * Adds the sortable columns.
  *
- * @param  array $sortable_columns
+ * @param array $sortable_columns Existing sortable columns.
  * @return array
  */
 function lsscms_columns_sortable( $sortable_columns ) {
@@ -59,11 +66,11 @@ function lsscms_columns_sortable( $sortable_columns ) {
 add_action( 'manage_sites_custom_column', 'lsscms_columns_content', 10, 2 );
 
 /**
- * Adds the content of the columns
+ * Adds the content of the columns.
  *
- * @global wpdb $wpdb WordPress database object
- * @param string $column_name
- * @param int $blog_id
+ * @global wpdb $wpdb WordPress database object.
+ * @param string $column_name Name of the column.
+ * @param int    $blog_id     ID of the blog.
  */
 function lsscms_columns_content( $column_name, $blog_id ) {
 	global $wpdb;
